829c480c7a3863f9670c5a036936eb7d247d2640
Qiqily0911 <59118457+Qiqily0911@users.noreply.github.com>
2020-11-27 02:14:39 +0800

    setting firebase config

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// firebase SDK
+const config = {
+  apiKey: "AIzaSyBmbHqONy7XHhbAJgi7XI8Zb6xrKcDQ9NM",
+  authDomain: "film-db-9936d.firebaseapp.com",
+  databaseURL: "https://film-db-9936d.firebaseio.com",
+  projectId: "film-db-9936d",
+  storageBucket: "film-db-9936d.appspot.com",
+  messagingSenderId: "340728739287",
+  appId: "1:340728739287:web:6a5a212fc24c2cd1b18792",
+};
+
+export default config;
