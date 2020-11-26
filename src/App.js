165d41dc75ecf09244ea6773af8d309c000c4266
Qiqily0911 <59118457+Qiqily0911@users.noreply.github.com>
2020-11-27 02:15:30 +0800

    load firebase realtime data for testing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import firebase from "firebase";
+import config from "./config";
+import "firebase/firestore";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    if (!firebase.apps.length) {
+      this.app = firebase.initializeApp(config);
+    } else {
+      this.app = firebase.app();
+    }
+    this.database = this.app.database();
+
+    this.state = {
+      read: "",
+    };
+    // this.name = "";
+  }
+
+  //  const [name, setName] = useState('');
+
+  handleChange = (e) => {
+    // setName(e.target.value);
+    console.log(e.target.value);
+  };
+
+  readData = () => {
+    const path = "/film_festival";
+    this.database.ref(path).once("value", (e) => {
+      const data = e.val();
+      console.log(data.cannes.palme_d_or[9]);
+      // this.setState({ read: data });
+    });
+  };
+
+  render() {
+    const { read } = this.state;
+    return (
+      <div>
+        <div>
+          <input
+            type="text"
+            name="text"
+            value={this.name}
+            onChange={() => this.handleChange}
+          />
+          <button type="button" onClick={() => this.readData()}>
+            read
+          </button>
+          <div>data: {read}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
